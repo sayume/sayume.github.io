18bd3a9bab03bbe273d66153ea257ee0f74df5b0
sayume <bilibili.fifth@gmail.com>
2014-11-06 10:30:36 +0800

    fix link redirect bug

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,25 @@
 /**
  * Created by sayume on 11/6/14.
  */
-(function(undefined,$){
+(function(undefined){
 
     'use strict';
 
     $(function(){
         //events
         $('#blogs').on('click',function(e){
-            $('#blogList').show();
 
-            return false;
+            e.preventDefault();
+
+            var blogList=$('#blogList');
+
+            if(blogList.is(':visible')){
+                blogList.hide();
+            }
+            else{
+                blogList.show();
+            }
+
         });
 
     });
